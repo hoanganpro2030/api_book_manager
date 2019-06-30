a6d9f8a856406cb9bcb2eaa6c0d93e22213d9f7a
hoanganpro2030 <1610132@hcmut.edu.vn>
2019-06-30 15:57:28 +0700

    update

--- tests/api/test_authors.py
+++ tests/api/test_authors.py
@@ -1,5 +1,5 @@
 from base_task.admin.models import Authors
-
+import json
 
 def test_get_all(client):
     response = client.get('/api/Authors/')
@@ -16,17 +16,110 @@ def test_by_id(client):
     response = client.delete('/api/Authors/3')
     assert response.status_code == 404
 
-def test_post_authors(client):
-    response = client.post('/api/Authors/', data={"last_name": "hoang thuong",
-                                                    "first_name": "dang",
-                                                    "phone": "0369327564",
-                                                    "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
-                                                    "id": 42,
-                                                    "email": "hoangthuong@gmail.com",
-                                                    "status": "true",
-                                                    "created": "Sat, 29 Jun 2019 13:15:35 -0000",
-                                                    "address": "Nha Trang"})
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    data = {
+        'last_name': "hoang thuong",
+        "first_name": "dang",
+        "phone": "0369327564",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong@gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.put('/api/Authors/2', data=json.dumps(data), headers=headers)
     assert response.status_code == 201
+    assert response.json['id'] == 2
+    assert response.json['phone'] == '0369327564'
+
+def test_post_authors(client):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    data = {
+        'last_name': "hoang thuong",
+        "first_name": "dang",
+        "phone": "0369327564",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong@gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.post('/api/Authors/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 201
+    assert response.json['email'] == "hoangthuong@gmail.com"
+
+    data = {
+        'last_name': "hoang thuong 1",
+        "first_name": "dang 1",
+        "phone": "0369327564",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong1@gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.post('/api/Authors/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['message'] == "Phone have already existed"
+
+    data = {
+        'last_name': "hoang thuong 1",
+        "first_name": "dang 1",
+        "phone": "0369327567",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong@gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.post('/api/Authors/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['message'] == "Email have already existed"
+
+    data = {
+        'last_name': "hoang thuong 2",
+        "phone": "0369327560",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong2@gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.post('/api/Authors/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['message'] == "Input payload validation failed"
+
+    data = {
+        'last_name': "hoang thuong 3",
+        "phone": "0369327563",
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "id": 42,
+        "email": "hoangthuong3gmail.com",
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "address": "Nha Trang"
+    }
+    response = client.post('/api/Authors/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['message'] == "Input payload validation failed"
+    response = client.get('/api/Authors/4')
+    assert response.status_code == 200
+    response = client.get('/api/Authors/5')
+    assert response.status_code == 404
+
 
 
 
--- tests/api/test_books.py
+++ tests/api/test_books.py
@@ -1,4 +1,4 @@
-
+import json
 
 def test_get_all(client):
     response = client.get('/api/Books/')
@@ -17,15 +17,115 @@ def test_by_id(client):
     response = client.get('/api/Books/search/list/')
     assert response.status_code == 404
 
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    data = {
+        'title': "sach 1",
+        "isbn": "978-604-1-00000-2",
+        "views": 5,
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "votes": 42,
+        "downloads": 0,
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "year": 2015,
+        "author_id": 1,
+        "id": 1
+    }
+    response = client.put('/api/Books/2', data=json.dumps(data), headers=headers)
+    assert response.status_code == 201
+    assert response.json['id'] == 2
+    assert response.json['isbn'] == '978-604-1-00000-2'
+
 
 # def test_post_books(client):
-#     response = client.post('/api/Authors/', data={"last_name": "hoang thuong",
-#                                                     "first_name": "dang",
-#                                                     "phone": "0369327564",
-#                                                     "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
-#                                                     "id": 42,
-#                                                     "email": "hoangthuong@gmail.com",
-#                                                     "status": "true",
-#                                                     "created": "Sat, 29 Jun 2019 13:15:35 -0000",
-#                                                     "address": "Nha Trang"})
+#     data = {
+#         'last_name': "hoang thuong",
+#         "first_name": "dang",
+#         "phone": "0369327564",
+#         "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+#         "id": 42,
+#         "email": "hoangthuong@gmail.com",
+#         "status": "true",
+#         "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+#         "address": "Nha Trang"
+#     }
+#     response = client.post('/api/Authors/', data=json.dumps(data))
 #     assert response.status_code == 201
+
+
+def test_post_authors(client):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    data = {
+        'title': "sach 1",
+        "isbn": "978-604-1-00000-2",
+        "views": 5,
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "votes": 42,
+        "downloads": 0,
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "year": 2015,
+        "author_id": 1,
+        "id": 1
+    }
+    response = client.post('/api/Books/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 201
+    assert response.json['isbn'] == "978-604-1-00000-2"
+
+    data = {
+        'title': "sach 2",
+        "isbn": "978-604-1-00000-2",
+        "views": 5,
+        "updated": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "votes": 42,
+        "downloads": 0,
+        "status": True,
+        "created": "Sat, 29 Jun 2019 13:15:35 -0000",
+        "year": 2015,
+        "author_id": 1,
+        "id": 1
+    }
+    response = client.post('/api/Books/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['message'] == "Invalid, ISBN already existed"
+
+    data = {
+        'title': "sach 3",
+        "isbn": "978-604-3-00000-2",
+        "year": 2015,
+        "author_id": 1
+    }
+    response = client.post('/api/Books/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 201
+    assert response.json['isbn'] == "978-604-3-00000-2"
+
+    data = {
+        "isbn": "978-604-4-00000-2",
+        "year": 2015,
+        "author_id": 1
+    }
+    response = client.post('/api/Books/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['errors']['title'] == "'title' is a required property"
+
+    data = {
+        'title': "sach 5",
+        "isbn": "978-604-5---000-2",
+        "year": 2015,
+        "author_id": 1,
+    }
+    response = client.post('/api/Books/', data=json.dumps(data), headers=headers)
+    assert response.status_code == 400
+    assert response.json['custom'] == "Validation of 'isbn' field failed"
+    response = client.get('/api/Books/5')
+    assert response.status_code == 200
+    response = client.get('/api/Books/6')
+    assert response.status_code == 404
